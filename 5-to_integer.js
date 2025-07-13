886faf17f5cc5a6a3d4fabf597e4024e6a439c44
MarcoIbrahim25 <marcoserry88@gmail.com>
2025-07-13 10:43:22 +0300

    Commit 7

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -3,7 +3,7 @@ const arg = process.argv[2];
 if (arg == !NaN) {
   console.log(arg);
 } else if (parseInt(arg) == !NaN) {
-  console.log(parseInt(arg));
+  console.log("Not a number");
 } else {
   console.log(arg);
 }
