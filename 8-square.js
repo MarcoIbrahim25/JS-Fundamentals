b319735ce8b5ebda4c2b88d569676cf7defbedfb
MarcoIbrahim25 <marcoserry88@gmail.com>
2025-07-13 14:09:51 +0300

    Commit 16

--- 8-square.js
+++ 8-square.js
@@ -1,8 +1,10 @@
 const arg = process.argv[2];
-if (isNaN(arg) || arg < 0) {
+const size = parseInt(arg);
+
+if (isNaN(size) || size < 0) {
   console.log("Missing size");
 } else {
-  for (let i = 0; i < arg; i++) {
-    console.log("x".repeat(arg));
+  for (let i = 0; i < size; i++) {
+    console.log("x".repeat(size));
   }
 }
