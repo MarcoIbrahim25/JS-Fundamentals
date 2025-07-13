cd40c0bd24cd082160415a15c39e6ab5869e94c7
MarcoIbrahim25 <marcoserry88@gmail.com>
2025-07-13 14:12:47 +0300

    Commit 17

--- 8-square.js
+++ 8-square.js
@@ -5,6 +5,6 @@ if (isNaN(size) || size < 0) {
   console.log("Missing size");
 } else {
   for (let i = 0; i < size; i++) {
-    console.log("x".repeat(size));
+    console.log("X".repeat(size));
   }
 }
