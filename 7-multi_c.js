33c88ecdc362db601817d75b7c33adabbf879b9c
MarcoIbrahim25 <marcoserry88@gmail.com>
2025-07-13 13:57:12 +0300

    Commit 13

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,7 +2,7 @@ const arg = process.argv[2];
 if (isNaN(arg)) {
   console.log("Missing number of occurrences");
 } else {
-  for (let i = 0; i < arg.length; i++) {
+  for (let i = 0; i < arg; i++) {
     console.log("C is fun");
   }
 }
